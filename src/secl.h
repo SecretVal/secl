6606bce3d704c3ceb4660260f33d26c94bba7617
secretval <108518296+SecretVal@users.noreply.github.com>
2025-02-09 15:05:15 +0100

    feat: secl_read_file

---
+++ src/secl.h
@@ -0,0 +1,21 @@
+#ifndef SECL_H
+#define SECL_H
+#include <stdio.h>
+#include <stdlib.h>
+
+char* secl_read_file(FILE *f)
+{
+  char* buf = 0;
+  long len;
+  if (f)  {
+    fseek(f, 0, SEEK_END);
+    len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    buf = malloc(len);
+    if (buf) {
+      fread(buf, 1, len, f);
+    }
+  }
+}
+
+#endif
